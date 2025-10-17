55e1a38310a3fa88cfbe7c7b0d7b9143e57bb0a6
Richard Beaver <rickybeaver3@gmail.com>
2025-10-16 20:10:32 -0400

    Initial Commit

---
+++ airpods_mouse/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  airpods_mouse
+//
+//  Created by Richard Beaver on 10/16/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
